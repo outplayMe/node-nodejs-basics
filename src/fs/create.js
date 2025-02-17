0dcd6961eed57330eb50104e8b7bf2435316c3f5
outplayMe <kirillpretty@gmail.com>
2025-02-17 22:54:55 +0300

    feat: implement fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,17 @@
+import { writeFile } from 'node:fs/promises';
+import path from 'node:path';
+
+const dirname = import.meta.dirname;
+const dest = path.join(dirname, 'files', 'fresh.txt');
+const data = 'I am fresh and young';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(dest, data, { flag: 'wx' });
+    console.log('File has been successfully created');
+  } catch (e) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
