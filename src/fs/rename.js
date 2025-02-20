cafd8857e1adf682acc5915e8acd8f1beba54953
outplayMe <kirillpretty@gmail.com>
2025-02-21 01:50:29 +0300

    feat: implement fs:rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,19 @@
-const rename = async () => {
-    // Write your code here 
+import { rename } from 'node:fs/promises';
+import path from 'node:path';
+
+const srcPath = path.join(import.meta.dirname, 'files', 'wrongFilename.txt');
+const renamedPath = path.join(
+  import.meta.dirname,
+  'files',
+  'properFilename.md'
+);
+
+const renameFile = async () => {
+  try {
+    await rename(srcPath, renamedPath);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
-await rename();
+await renameFile();
