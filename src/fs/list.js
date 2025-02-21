1ebc9a8e5b8637865d54b6dd8983b7a41bf56939
outplayMe <kirillpretty@gmail.com>
2025-02-21 18:22:51 +0300

    feat: implement fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { readdir } from 'node:fs/promises';
+import path from 'node:path';
+
+const srcPath = path.join(import.meta.dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(srcPath, { recursive: true });
+    console.log(files);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
