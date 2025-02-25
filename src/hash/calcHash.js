54b05dd23b7a9652d238a82ef59f1c1e34918432
outplayMe <kirillpretty@gmail.com>
2025-02-25 18:23:04 +0300

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+import path from 'node:path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+
+  const input = createReadStream(
+    path.join(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt')
+  );
+
+  input.on('readable', () => {
+    const data = input.read();
+
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log('sha-256 hash of your file:\n', hash.digest('hex'));
+    }
+  });
 };
 
 await calculateHash();
