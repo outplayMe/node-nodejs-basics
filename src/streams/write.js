efba2251e2102e2ca8e13cab8f486c4a2ccc9a4e
outplayMe <kirillpretty@gmail.com>
2025-02-25 23:53:26 +0300

    feat: implement streams:write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,15 @@
+import { createWriteStream } from 'node:fs';
+import path from 'node:path';
+import { stdin } from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+  const writable = createWriteStream(
+    path.join(import.meta.dirname, 'files', 'fileToWrite.txt')
+  );
+
+  stdin.on('data', (userInput) => {
+    writable.write(userInput.toString());
+  });
 };
 
 await write();
