6b5842d8eea3e853e71ff9b5e7a24cfd34780f15
outplayMe <kirillpretty@gmail.com>
2025-02-25 23:37:09 +0300

    feat: implement streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+  const readable = createReadStream(
+    path.join(import.meta.dirname, 'files', 'fileToRead.txt')
+  );
+
+  readable.on('data', (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  readable.on('end', () => {
+    process.stdout.write('\n');
+  });
 };
 
 await read();
