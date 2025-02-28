acc20a3659cd5aedde9331f6634d2169166f1818
outplayMe <kirillpretty@gmail.com>
2025-02-28 13:07:25 +0300

    implement both zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import path from 'node:path';
+
 const compress = async () => {
-    // Write your code here 
+  const source = createReadStream(
+    path.join(import.meta.dirname, 'files', 'fileToCompress.txt')
+  );
+  const dest = createWriteStream(
+    path.join(import.meta.dirname, 'files', 'archive.gz')
+  );
+  const gzip = createGzip();
+
+  await pipeline(source, gzip, dest);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createUnzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import path from 'node:path';
+
 const decompress = async () => {
-    // Write your code here 
+  const source = createReadStream(
+    path.join(import.meta.dirname, 'files', 'archive.gz')
+  );
+  const dest = createWriteStream(
+    path.join(import.meta.dirname, 'files', 'fileToCompress.txt')
+  );
+  const unzip = createUnzip();
+
+  await pipeline(source, unzip, dest);
 };
 
 await decompress();
