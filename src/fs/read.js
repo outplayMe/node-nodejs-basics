9303ac820632bf30775502dd7f77f540d965bdbc
outplayMe <kirillpretty@gmail.com>
2025-03-29 16:38:31 +0300

    feat: implement read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(import.meta.dirname, 'files', 'fileToRead.txt');
+
+  try {
+    const contents = await readFile(filePath, { encoding: 'utf-8' });
+    console.log(contents);
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
