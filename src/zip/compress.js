875a76512a1e10f078bef247b22c7955a29e8b56
outplayMe <kirillpretty@gmail.com>
2025-04-03 00:10:50 +0300

    feat: implement compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createWriteStream, createReadStream } from 'node:fs';
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import path from 'node:path';
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream(
+    path.join(import.meta.dirname, 'files', 'fileToCompress.txt')
+  );
+  const dest = createWriteStream(
+    path.join(import.meta.dirname, 'files', 'archive.gz')
+  );
+  await pipeline(source, gzip, dest);
 };
 
 await compress();
